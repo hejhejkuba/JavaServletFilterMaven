d90a57cf488b536338e26b6a5f8af2d6419a5ce3
Jakub Litwin <jakub01lit@gmail.com>
2023-04-03 17:51:46 +0200

    Initial commit

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/hello.world")
+public class HelloWorldServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String abcParam = request.getParameter("abcParam");
+        String message = "Hello World";
+
+        // wypisanie wartości parametru "abc" przy użyciu filtru
+        if (abcParam == null) {
+            System.out.println(abcParam);
+            // wypisanie "Hello World"
+            response.setContentType("text/html");
+            response.getWriter().println("<h1>" + message + "</h1>");
+        }
+
+    }
+}
+
